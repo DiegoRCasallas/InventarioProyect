4829032252be85936c4e29ba749fccd9c5b8824e
juancamacho18 <126798122+juancamacho18@users.noreply.github.com>
2024-05-27 15:53:14 -0500

    Add files via upload

---
+++ InventarioProyect-main/inventarios/src/main/java/com/inventario/inventarios/controller/usuarioController.java
@@ -0,0 +1,5 @@
+package com.inventario.inventarios.controller;
+
+public class usuarioController {
+    
+}
---
+++ InventarioProyect-main/inventarios/src/main/java/com/inventario/inventarios/model/Proveedor.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.inventario.inventarios.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Labing-PC
+ */
+public class Proveedor implements Serializable{
+    private int id;
+    private String nombre;
+    private int telefono;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+    
+}
