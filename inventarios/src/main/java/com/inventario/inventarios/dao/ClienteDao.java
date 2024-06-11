1df0ff6ac837817c91a1102291eac253fcf268d3
Diego Casallas <master.drcd@gmail.com>
2024-06-10 21:00:56 -0500

    reparacion rama main

---
+++ inventarios/src/main/java/com/inventario/inventarios/dao/ClienteDao.java
@@ -0,0 +1,64 @@
+package com.inventario.inventarios.dao;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.inventario.inventarios.model.Cliente;
+import com.inventario.inventarios.model.Proveedor;
+
+
+public class ClienteDao {
+    private List<Cliente> listaClientes;
+    private ObjectInputStream entrada;
+    private ObjectOutputStream salida;
+    private String archivo="clientes.dat";
+
+    public ClienteDao(){
+        listaClientes=new ArrayList<>();
+        
+        File file=new File(archivo);
+        
+       
+        if(file.isFile()){
+            try{
+                this.entrada=new ObjectInputStream(new FileInputStream("clientes.dat"));
+                this.listaClientes=(List<Cliente>) entrada.readObject();
+                this.entrada.close();
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                    guardar();
+            }
+        }
+    }
+    
+    public boolean crear(Cliente cliente){
+        listaClientes.add(cliente);
+        guardar();
+        return true;
+    }
+    
+    private void guardar() {
+        try{
+            this.salida=new ObjectOutputStream(new FileOutputStream("clientes.dat"));
+            this.salida.writeObject(listaClientes);
+            this.salida.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void borrar(Cliente cliente){
+        listaClientes.remove(cliente);
+        guardar();
+    }
+
+    
+    public List<Cliente> mostrarTodos(){
+        return listaClientes;
+    }
+}
